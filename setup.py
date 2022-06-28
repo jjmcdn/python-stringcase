6d8fa7393d5d485c46bb7b0120a9d11f2804b2ff
Joe MacDonald <joe_macdonald@mentor.com>
2022-06-28 14:32:22 -0400

    switch to pypi version
    Signed-off-by: Joe MacDonald <joe_macdonald@mentor.com>

--- setup.py
+++ setup.py
@@ -1,4 +1,15 @@
-from setuptools import setup
+from distutils.core import setup
 
-if __name__ == "__main__":
-    setup()
+setup(
+    name='stringcase',
+    version='1.2.0',
+    py_modules=[
+        'stringcase'
+    ],
+    url='https://github.com/okunishinishi/python-stringcase',
+    license='MIT',
+    author='Taka Okunishi',
+    author_email='okunishitaka.com@gmail.com',
+    description='String case converter.',
+    long_description=open('./README.rst').read()
+)
